9dddcfd9493bc82696e9c7d734544ec70e58c6ca
ahsonkhan <ahson_ahmedk@yahoo.com>
2015-10-09 12:53:57 -0700

    Remove unnecessary using statement.

--- src/Microsoft.DotNet.Cli/Program.cs
+++ src/Microsoft.DotNet.Cli/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Dnx.Runtime.Common.CommandLine;
 using Microsoft.DotNet.Cli.Utils;
 
