663900df3b53d6682ad109a43a581b517973a538
Sridhar Periyasamy <sridhper@microsoft.com>
2015-10-12 15:25:54 -0700

    Fix build script to build on linux CI machines
     - Download nuget.exe into the '.nuget' folder directly
     - Use appropriate myget feed to download Korebuild package

--- build.sh
+++ build.sh
@@ -1,31 +1,21 @@
 #!/usr/bin/env bash
 
-if test `uname` = Darwin; then
-    cachedir=~/Library/Caches/KBuild
-else
-    if [ -z $XDG_DATA_HOME ]; then
-        cachedir=$HOME/.local/share
-    else
-        cachedir=$XDG_DATA_HOME;
-    fi
-fi
-mkdir -p $cachedir
 nugetVersion=latest
-cachePath=$cachedir/nuget.$nugetVersion.exe
+nugetPath=.nuget/nuget.exe
 
 url=https://dist.nuget.org/win-x86-commandline/$nugetVersion/nuget.exe
 
-if test ! -f $cachePath; then
-    wget -O $cachePath $url 2>/dev/null || curl -o $cachePath --location $url /dev/null
-fi
-
 if test ! -e .nuget; then
     mkdir .nuget
-    cp $cachePath .nuget/nuget.exe
 fi
 
+if test ! -f $nugetPath; then
+    wget -O $nugetPath $url 2>/dev/null || curl -o $nugetPath --location $url /dev/null
+fi
+
+
 if test ! -d packages/KoreBuild; then
-    mono .nuget/nuget.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
+    mono .nuget/nuget.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre -source https://www.myget.org/F/aspnetvnext/api/v3/index.json
     mono .nuget/nuget.exe install Sake -ExcludeVersion -Out packages
 fi
 
