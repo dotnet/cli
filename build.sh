e83dfec50f43aad6703f0bfc67138709721e4197
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2015-10-21 15:21:14 -0700

    set a value for HOME if not already set
    works around a DNX issue

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-./scripts/bootstrap.sh
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-./scripts/package.sh
+# Some things depend on HOME and it may not be set. We should fix those things, but until then, we just patch a value in
+if [ -z "$HOME" ]; then
+    export HOME=$DIR/artifacts/home
+    mkdir -p $HOME
+fi
+
+$DIR/scripts/bootstrap.sh
+$DIR/scripts/package.sh
