18f427bceaf8f5eae9f6b0266300da45c5df2cfe
Bryan <brthor@microsoft.com>
2015-10-23 14:13:43 -0700

    debug ci postbuild

--- scripts/ci_postbuild.sh
+++ scripts/ci_postbuild.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo $(ls)
+
 if [[ "$(uname)" == "Linux" ]]; then
-    ./dockerpostbuild.sh $@
+    $SCRIPT_DIR/dockerpostbuild.sh $@
 fi
 
 ret_code=$?
