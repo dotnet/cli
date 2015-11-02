851ad8a5b1884ca893d42d1d7a9ea2b191b669b4
Geoff Norton <grompf@gmail.com>
2015-11-02 14:27:29 -0800

    [osx] Detect the major/minor at runtime, instead of hard coding it

--- scripts/dotnet-restore.sh
+++ scripts/dotnet-restore.sh
@@ -12,4 +12,11 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 # work around restore timeouts on Mono
 [ -z "$MONO_THREADS_PER_CPU" ] && export MONO_THREADS_PER_CPU=50
 
-exec "$DIR/dnx/dnx" "$DIR/dnx/lib/Microsoft.Dnx.Tooling/Microsoft.Dnx.Tooling.dll" "restore" --runtime "osx.10.10-x64" --runtime "ubuntu.14.04-x64" "$@"
+if [ "$(uname)" == "Darwin" ]; then
+  MAJOR_MAC_VERSION=$(sw_vers -productVersion | awk -F '.' '{print $1 "." $2}')
+  RUNTIME="osx.${MAJOR_MAC_VERSION}-x64"
+else
+  RUNTIME="ubuntu.14.04-x64"
+fi
+
+exec "$DIR/dnx/dnx" "$DIR/dnx/lib/Microsoft.Dnx.Tooling/Microsoft.Dnx.Tooling.dll" "restore" --runtime ${RUNTIME} "$@"
