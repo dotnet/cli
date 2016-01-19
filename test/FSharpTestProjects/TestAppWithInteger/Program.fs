04d773a81333d88a2b275abc0da65a3d0bcb35a9
Enrico Sada <enrico@sada.io>
2016-01-19 13:39:49 +0100

    add --targetprofile:netcore if target framework is not desktop
    update FSharpTestProjects, like test/TestProjects

---
+++ test/FSharpTestProjects/TestAppWithInteger/Program.fs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace TestAppWithInteger
+
+open System
+
+module Program =
+
+    [<EntryPoint>]
+    let Main args =
+        if 1 < 2 then 0 else 1
