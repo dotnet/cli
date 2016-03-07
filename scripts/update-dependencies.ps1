92819a93b551c2a0bf9be4ebfb6d8e40ecadc217
Eric Erhardt <eric.erhardt@microsoft.com>
2016-03-07 15:01:21 -0600

    Fix script in VSO by adding -UseBasicParsing

--- scripts/update-dependencies.ps1
+++ scripts/update-dependencies.ps1
@@ -3,6 +3,6 @@
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 #
 
-$CoreFxLkgVersion = Invoke-WebRequest https://raw.githubusercontent.com/eerhardt/versions/master/corefx/release/1.0.0-rc2/LKG.txt
+$CoreFxLkgVersion = Invoke-WebRequest https://raw.githubusercontent.com/eerhardt/versions/master/corefx/release/1.0.0-rc2/LKG.txt -UseBasicParsing
 
 Write-Host "got this $CoreFxLkgVersion"
