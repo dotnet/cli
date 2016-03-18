b8945367e5d7ffb80e0620cda57c1387f5ae0e2e
Senthil <schellap@microsoft.com>
2016-03-18 16:22:11 -0700

    Package corehost

--- src/corehost/cli/fxr/hostfxr.cpp
+++ src/corehost/cli/fxr/hostfxr.cpp
@@ -75,25 +75,29 @@ int execute_app(
 
 bool hostpolicy_exists_in_svc(pal::string_t* resolved_dir)
 {
-#ifdef COREHOST_PACKAGE_SERVICING
     pal::string_t svc_dir;
     if (!pal::getenv(_X("DOTNET_SERVICING"), &svc_dir))
     {
         return false;
     }
 
+    pal::string_t rel_dir = _STRINGIFY(HOST_POLICY_PKG_REL_DIR);
+    if (DIR_SEPARATOR != '/')
+    {
+        std::replace(rel_dir.begin(), rel_dir.end(), '/', DIR_SEPARATOR);
+    }
+
     pal::string_t path = svc_dir;
-    append_path(&path, COREHOST_PACKAGE_NAME);
-    append_path(&path, COREHOST_PACKAGE_VERSION);
-    append_path(&path, COREHOST_PACKAGE_COREHOST_RELATIVE_DIR);
-    if (library_exists_in_dir(path, LIBHOST_NAME))
+    append_path(&path, _STRINGIFY(HOST_POLICY_PKG_NAME));
+    append_path(&path, _STRINGIFY(HOST_POLICY_PKG_VER));
+    append_path(&path, rel_dir.c_str());
+    append_path(&path, LIBHOST_NAME);
+    if (library_exists_in_dir(path, LIBHOST_NAME, nullptr))
     {
         resolved_dir->assign(path);
+        return true;
     }
-    return true;
-#else
     return false;
-#endif
 }
 
 SHARED_API int hostfxr_main(const int argc, const pal::char_t* argv[])
--- src/corehost/cli/hostpolicy.cpp
+++ src/corehost/cli/hostpolicy.cpp
@@ -207,7 +207,9 @@ SHARED_API int corehost_main(const int argc, const pal::char_t* argv[])
 
     if (trace::is_enabled())
     {
-        trace::info(_X("--- Invoked policy main = {"));
+        trace::info(_X("--- Invoked policy [%s.%s] main = {"),
+            _STRINGIFY(HOST_POLICY_PKG_NAME), _STRINGIFY(HOST_POLICY_PKG_VER));
+
         for (int i = 0; i < argc; ++i)
         {
             trace::info(_X("%s"), argv[i]);
