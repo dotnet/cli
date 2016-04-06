163259cbd2c73696c56c3e0070998f6624aa43eb
Pavel Krymets <pavel@krymets.com>
2016-04-06 10:35:55 -0700

    Alow type platform to be in target dependencies

--- src/Microsoft.DotNet.ProjectModel/ProjectContext.cs
+++ src/Microsoft.DotNet.ProjectModel/ProjectContext.cs
@@ -145,8 +145,10 @@ namespace Microsoft.DotNet.ProjectModel
             {
                 return this;
             }
-            
-            var standalone = !ProjectFile.Dependencies.Any(d => d.Type.Equals(LibraryDependencyType.Platform));
+
+            var standalone = !ProjectFile.Dependencies
+                .Concat(ProjectFile.GetTargetFramework(TargetFramework).Dependencies)
+                .Any(d => d.Type.Equals(LibraryDependencyType.Platform));
 
             var context = CreateBuilder(ProjectFile.ProjectFilePath, TargetFramework)
                 .WithRuntimeIdentifiers(standalone ? runtimeIdentifiers : Enumerable.Empty<string>())
