607f89cef7b34e27b03604980f9c34d0f03a2fc6
Senthil <schellap@microsoft.com>
2016-04-12 15:03:36 -0700

    Servicing for hostpolicy

--- src/corehost/cli/fxr/hostfxr.cpp
+++ src/corehost/cli/fxr/hostfxr.cpp
@@ -87,6 +87,7 @@ bool hostpolicy_exists_in_svc(pal::string_t* resolved_dir)
 {
     pal::string_t svc_dir;
     pal::get_default_extensions_directory(&svc_dir);
+    append_path(&svc_dir, _X("msil"));
 
     pal::string_t version = _STRINGIFY(HOST_POLICY_PKG_VER);
 
@@ -104,19 +105,7 @@ bool hostpolicy_exists_in_svc(pal::string_t* resolved_dir)
 
     pal::string_t path = svc_dir;
     append_path(&path, _STRINGIFY(HOST_POLICY_PKG_NAME));
-
-    pal::string_t max_ver;
-    if (lib_ver.is_prerelease())
-    {
-        try_prerelease_roll_forward_in_dir(path, lib_ver, &max_ver);
-    }
-    else
-    {
-        try_patch_roll_forward_in_dir(path, lib_ver, &max_ver);
-    }
-
-
-    append_path(&path, max_ver.c_str());
+    append_path(&path, version.c_str());
     append_path(&path, rel_dir.c_str());
 
     if (library_exists_in_dir(path, LIBHOSTPOLICY_NAME, nullptr))
