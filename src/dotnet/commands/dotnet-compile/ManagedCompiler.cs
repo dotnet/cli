e1ca23a7729b1999434141765931ddd3b6b0b33b
Eric Erhardt <eric.erhardt@microsoft.com>
2016-04-14 23:33:41 -0500

    Invoke compile-csc in-proc
    Since processes are expensive, suppress spawning a new process when dotnet-compile is invoking dotnet-compile-csc.

--- src/dotnet/commands/dotnet-compile/ManagedCompiler.cs
+++ src/dotnet/commands/dotnet-compile/ManagedCompiler.cs
@@ -1,11 +1,14 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.DotNet.Cli;
 using Microsoft.DotNet.Cli.Compiler.Common;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.DotNet.ProjectModel;
@@ -81,7 +84,7 @@ namespace Microsoft.DotNet.Tools.Compiler
             var compilationOptions = context.ResolveCompilationOptions(args.ConfigValue);
 
             // Set default platform if it isn't already set and we're on desktop
-            if(compilationOptions.EmitEntryPoint == true && string.IsNullOrEmpty(compilationOptions.Platform) && context.TargetFramework.IsDesktop())
+            if (compilationOptions.EmitEntryPoint == true && string.IsNullOrEmpty(compilationOptions.Platform) && context.TargetFramework.IsDesktop())
             {
                 // See https://github.com/dotnet/cli/issues/2428 for more details.
                 compilationOptions.Platform = RuntimeInformation.ProcessArchitecture == Architecture.X64 ?
@@ -181,31 +184,45 @@ namespace Microsoft.DotNet.Tools.Compiler
 
             _scriptRunner.RunScripts(context, ScriptNames.PreCompile, contextVariables);
 
-            var result = _commandFactory.Create($"compile-{compilerName}", new[] { "@" + $"{rsp}" })
-                .OnErrorLine(line =>
+            string commandName = $"compile-{compilerName}";
+            string commandArg = $"@{rsp}";
+
+            CommandResult result;
+            Func<string[], int> builtInCommand;
+            if (Program.TryGetBuiltInCommand(commandName, out builtInCommand))
+            {
+                TextWriter originalConsoleOut = Console.Out;
+                TextWriter originalConsoleError = Console.Error;
+                try
                 {
-                    var diagnostic = ParseDiagnostic(context.ProjectDirectory, line);
-                    if (diagnostic != null)
-                    {
-                        diagnostics.Add(diagnostic);
-                    }
-                    else
-                    {
-                        Reporter.Error.WriteLine(line);
-                    }
-                })
-                .OnOutputLine(line =>
+                    // redirect the standard out and error so we can parse the diagnostics
+                    var outputWriter = new LineNotificationTextWriter(originalConsoleOut.FormatProvider, originalConsoleOut.Encoding)
+                        .OnWriteLine(line => HandleCompilerOutputLine(line, context, diagnostics, Reporter.Output));
+                    Console.SetOut(outputWriter);
+
+                    var errorWriter = new LineNotificationTextWriter(originalConsoleError.FormatProvider, originalConsoleError.Encoding)
+                        .OnWriteLine(line => HandleCompilerOutputLine(line, context, diagnostics, Reporter.Error));
+                    Console.SetError(errorWriter);
+
+                    int exitCode = builtInCommand(new[] { commandArg });
+
+                    // fake out a ProcessStartInfo for the reporting code below
+                    ProcessStartInfo startInfo = new ProcessStartInfo(new Muxer().MuxerPath, $"{commandName} {commandArg}");
+                    result = new CommandResult(startInfo, exitCode, null, null);
+                }
+                finally
                 {
-                    var diagnostic = ParseDiagnostic(context.ProjectDirectory, line);
-                    if (diagnostic != null)
-                    {
-                        diagnostics.Add(diagnostic);
-                    }
-                    else
-                    {
-                        Reporter.Output.WriteLine(line);
-                    }
-                }).Execute();
+                    Console.SetOut(originalConsoleOut);
+                    Console.SetError(originalConsoleError);
+                }
+            }
+            else
+            {
+                result = _commandFactory.Create(commandName, new[] { commandArg })
+                    .OnErrorLine(line => HandleCompilerOutputLine(line, context, diagnostics, Reporter.Error))
+                    .OnOutputLine(line => HandleCompilerOutputLine(line, context, diagnostics, Reporter.Output))
+                    .Execute();
+            }
 
             // Run post-compile event
             contextVariables["compile:CompilerExitCode"] = result.ExitCode.ToString();
@@ -224,6 +241,77 @@ namespace Microsoft.DotNet.Tools.Compiler
             }
 
             return PrintSummary(diagnostics, sw, success);
+        }
+
+        private static void HandleCompilerOutputLine(string line, ProjectContext context, List<DiagnosticMessage> diagnostics, Reporter reporter)
+        {
+            var diagnostic = ParseDiagnostic(context.ProjectDirectory, line);
+            if (diagnostic != null)
+            {
+                diagnostics.Add(diagnostic);
+            }
+            else
+            {
+                reporter.WriteLine(line);
+            }
+        }
+
+        /// <summary>
+        /// A TextWriter that can raises an event for each line that is written to it.
+        /// </summary>
+        private class LineNotificationTextWriter : TextWriter
+        {
+            private Encoding _encoding;
+            private StringBuilder _currentString;
+            private Action<string> _lineHandler;
+
+            public LineNotificationTextWriter(IFormatProvider formatProvider, Encoding encoding)
+                : base(formatProvider)
+            {
+                _encoding = encoding;
+
+                // start with an average line length so the builder doesn't need to immediately grow
+                _currentString = new StringBuilder(128);
+            }
+
+            public LineNotificationTextWriter OnWriteLine(Action<string> lineHandler)
+            {
+                Debug.Assert(lineHandler != null);
+                Debug.Assert(_lineHandler == null);
+
+                _lineHandler = lineHandler;
+
+                return this;
+            }
+
+            public override Encoding Encoding
+            {
+                get { return _encoding; }
+            }
+
+            public override void Write(char value)
+            {
+                string currentLine = null;
+
+                lock (_currentString)
+                {
+                    if (value == '\n')
+                    {
+                        currentLine = _currentString.ToString();
+                        _currentString.Clear();
+                    }
+                    else
+                    {
+                        _currentString.Append(value);
+                    }
+                }
+
+                if (currentLine != null)
+                {
+                    // invoke the handler outside of the lock
+                    _lineHandler?.Invoke(currentLine);
+                }
+            }
         }
     }
 }
