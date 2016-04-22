b56b7b81f284d0f1c1d4bf3dcd51f29588913ae5
Eric Erhardt <eric.erhardt@microsoft.com>
2016-04-22 14:20:50 -0500

    Adding all code pages for dotnet CLI.
    Since we write to the console and create new processes and read from their stdout and stderr, we need to ensure we have all the code pages in our process.  This way we can encode/decode strings correctly when writing to our Console, and when reading from spawned process's output.
    
    Fix #2486

--- src/dotnet/Program.cs
+++ src/dotnet/Program.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.DotNet.ProjectModel.Server;
 using Microsoft.DotNet.Tools.Build;
@@ -40,6 +41,8 @@ namespace Microsoft.DotNet.Cli
         public static int Main(string[] args)
         {
             DebugHelper.HandleDebugSwitch(ref args);
+
+            InitializeProcess();
 
             try
             {
@@ -136,6 +139,16 @@ namespace Microsoft.DotNet.Cli
 
             return exitCode;
 
+        }
+
+        /// <summary>
+        /// Performs initialization that must be done before anything else in the process.
+        /// </summary>
+        private static void InitializeProcess()
+        {
+            // by default, .NET Core doesn't have all code pages needed for Console apps.
+            // see the .NET Core Notes in https://msdn.microsoft.com/en-us/library/system.diagnostics.process(v=vs.110).aspx
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         }
 
         internal static bool TryGetBuiltInCommand(string commandName, out Func<string[], int> builtInCommand)
