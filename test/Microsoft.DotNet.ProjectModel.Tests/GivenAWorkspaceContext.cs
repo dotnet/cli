7a082eac844fc34659b4f23476c0fe3be00cc76e
Eric Erhardt <eric.erhardt@microsoft.com>
2016-04-26 17:22:09 -0500

    Add a test for WorkspaceContext.GetProjectContextCollection("project.json").

---
+++ test/Microsoft.DotNet.ProjectModel.Tests/GivenAWorkspaceContext.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+using FluentAssertions;
+using Microsoft.DotNet.TestFramework;
+using Microsoft.DotNet.Tools.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.DotNet.ProjectModel.Tests
+{
+    public class GivenAWorkspaceContext : TestBase
+    {
+        [Fact]
+        public void GetProjectContextCollectionWorksForALocalProjectJson()
+        {
+            string projectJsonFileName = Project.FileName;
+
+            // normally the test will be run in the test directory - which already has a project.json file.
+            // But, if a local project.json file doesn't exist, copy one locally
+            if (!File.Exists(projectJsonFileName))
+            {
+                string projectJsonPath = Path.Combine(TestAssetsManager.AssetsRoot, "TestAppSimple", projectJsonFileName);
+                File.Copy(projectJsonPath, projectJsonFileName, true);
+            }
+
+            WorkspaceContext context = WorkspaceContext.Create(designTime: false);
+            ProjectContextCollection contexts = context.GetProjectContextCollection(projectJsonFileName);
+
+            contexts.Should().NotBeNull();
+            contexts.Project.Should().NotBeNull();
+            contexts.Project.Name.Should().Be(new DirectoryInfo(Directory.GetCurrentDirectory()).Name);
+        }
+    }
+}
