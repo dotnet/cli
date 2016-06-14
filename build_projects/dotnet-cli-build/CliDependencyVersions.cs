1959209599dbcc8dd49e545dadea30ecd031fbe0
schellap <schellap@microsoft.com>
2016-06-13 19:58:25 -0700

    Update dependencies to latest

--- build_projects/dotnet-cli-build/CliDependencyVersions.cs
+++ build_projects/dotnet-cli-build/CliDependencyVersions.cs
@@ -7,9 +7,9 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class CliDependencyVersions
     {
-        public static readonly string SharedFrameworkVersion = "1.0.0-rc3-004465-00";
-        public static readonly string SharedHostVersion = "1.0.1-rc3-004465-00";
-        public static readonly string HostFxrVersion = "1.0.1-rc3-004465-00";
+        public static readonly string SharedFrameworkVersion = "1.0.0";
+        public static readonly string SharedHostVersion = "1.0.1";
+        public static readonly string HostFxrVersion = "1.0.1";
 
         public static readonly string SharedFrameworkChannel = "preview";
         public static readonly string SharedHostChannel = "preview";
