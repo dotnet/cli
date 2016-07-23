b45685f3a229910bf9761fc5d7b898be0d8af091
Piotr Puszkiewicz <piotrp@microsoft.com>
2016-07-23 11:42:38 -0700

    Decompose Crossgen Task

---
+++ build_projects/dotnet-cli-build/GetNuGetInformation.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.DotNet.Cli.Build;
+
+namespace Microsoft.DotNet.Cli.Build
+{
+    public class GetNuGetInformation : Task
+    {
+        [Output]
+        public string PackagesDir { get; set; }
+
+        public override bool Execute()
+        {
+            PackagesDir = Dirs.NuGetPackages;
+
+            return true;
+        }
+    }
+}
