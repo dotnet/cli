d66c43e9c46cb04041a68da30507081fc31b08dc
Eric Mellino <erme@microsoft.com>
2016-09-28 11:56:11 -0700

    Fix versions

--- build_projects/shared-build-targets-utils/Publishing/AzurePublisher.cs
+++ build_projects/shared-build-targets-utils/Publishing/AzurePublisher.cs
@@ -187,7 +187,7 @@ namespace Microsoft.DotNet.Cli.Build
         public static async Task DownloadFile(string blobFilePath, string localDownloadPath)
         {
             var blobUrl = $"{s_dotnetBlobRootUrl}{blobFilePath}";
-
+            Console.WriteLine($"Downloading {blobUrl} to {localDownloadPath}.");
             using (var client = new HttpClient())
             {
                 var request = new HttpRequestMessage(HttpMethod.Get, blobUrl);
