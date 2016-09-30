25e752f5e3546fa3e7e17204fc558b4f0df33d7f
Eric Mellino <erme@microsoft.com>
2016-09-30 15:19:16 -0700

    Update to version that actually successfully published all archives

--- build_projects/dotnet-cli-build/CliDependencyVersions.cs
+++ build_projects/dotnet-cli-build/CliDependencyVersions.cs
@@ -7,9 +7,9 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class CliDependencyVersions
     {
-        public static readonly string SharedFrameworkVersion = "1.1.0-preview1-001073-00";
-        public static readonly string SharedHostVersion = "1.1.0-preview1-001073-00";
-        public static readonly string HostFxrVersion = "1.1.0-preview1-001073-00";
+        public static readonly string SharedFrameworkVersion = "1.1.0-preview1-001086-00";
+        public static readonly string SharedHostVersion = "1.1.0-preview1-001086-00";
+        public static readonly string HostFxrVersion = "1.1.0-preview1-001086-00";
 
         public static readonly string SharedFrameworkChannel = "release/1.1.0";
         public static readonly string SharedHostChannel = "release/1.1.0";
