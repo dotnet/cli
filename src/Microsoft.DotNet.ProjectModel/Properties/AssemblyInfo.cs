9b3f5eb41357e4c597ed95ece4b15a7d3d4d349a
Justin Goshi <jgoshi@microsoft.com>
2016-10-20 00:54:44 +0000

    Update Microsoft.NET.Sdk version. Needed a bug fix for correct TargetFramework version for project dependencies.

--- src/Microsoft.DotNet.ProjectModel/Properties/AssemblyInfo.cs
+++ src/Microsoft.DotNet.ProjectModel/Properties/AssemblyInfo.cs
@@ -8,7 +8,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
