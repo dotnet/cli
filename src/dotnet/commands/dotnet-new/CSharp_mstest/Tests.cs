5815160502fd08d7c8c62205545ce015ce8e82fd
Justin Goshi <jgoshi@microsoft.com>
2016-11-01 13:19:35 -0700

    WIP adding new mstest templates

---
+++ src/dotnet/commands/dotnet-new/CSharp_mstest/Tests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void TestMethodPassing()
+        {
+            Assert.IsTrue(true);
+        }
+
+    }
+}
