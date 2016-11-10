022ec662f92ed25e79ff4b0904536e10f07ec50e
eerhardt <eric.erhardt@microsoft.com>
2016-11-10 01:05:27 +0000

    Updating CoreFx, CoreClr, CoreSetup to 1.0.2, 1.0.5, 1.0.3 respectively

--- build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -7,7 +7,7 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class DependencyVersions
     {
-        public static readonly string CoreCLRVersion = "1.0.5-servicing-24704-01";
-        public static readonly string JitVersion = "1.0.5-servicing-24704-01";
+        public static readonly string CoreCLRVersion = "1.0.5";
+        public static readonly string JitVersion = "1.0.5";
     }
 }
