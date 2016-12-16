6b705f206a08650597d2ed2a46dfcefa41c7d197
Justin Goshi <jgoshi@microsoft.com>
2016-12-16 15:41:50 -0800

    Simple migration test

--- test/dotnet-migrate.Tests/GivenThatIWantToMigrateSolutions.cs
+++ test/dotnet-migrate.Tests/GivenThatIWantToMigrateSolutions.cs
@@ -2,9 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using FluentAssertions;
-using Xunit;
 using Microsoft.DotNet.TestFramework;
 using Microsoft.DotNet.Tools.Test.Utilities;
+using System.IO;
+using Xunit;
 
 namespace Microsoft.DotNet.Migration.Tests
 {
@@ -13,20 +14,17 @@ namespace Microsoft.DotNet.Migration.Tests
         [Fact]
         public void ItMigratesSln()
         {
-            //var rootDirectory = TestAssetsManager.CreateTestInstance(
-            //    "TestAppWithSlnAndMultipleProjects",
-            //    callingMethod: "a").Path;
+            var projectDirectory = TestAssets
+                .Get("TestAppWithSlnAndMultipleProjects")
+                .CreateInstance()
+                .WithSourceFiles()
+                .Root
+                .FullName;
 
-            //var testAppProjectDirectory = Path.Combine(rootDirectory, "TestApp");
-            //var testLibProjectDirectory = Path.Combine(rootDirectory, "TestLibrary");
-            //string slnPath = Path.Combine(testAppProjectDirectory, "TestApp.sln");
-            
-            //CleanBinObj(testAppProjectDirectory);
-            //CleanBinObj(testLibProjectDirectory);
-
-            //MigrateProject(slnPath);
-            //Restore(testAppProjectDirectory, "TestApp.csproj");
-            //BuildMSBuild(testAppProjectDirectory, "TestApp.sln", "Release");
+            var cmd = new DotnetCommand()
+                .WithWorkingDirectory(projectDirectory)
+                .ExecuteWithCapturedOutput($"migrate \"{Path.Combine("TestApp", "TestApp.sln")}\"");
+            cmd.Should().Pass();
         }
     }
 }
