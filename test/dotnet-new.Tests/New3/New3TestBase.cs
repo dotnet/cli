3d21636f65f167c620a883e37cefde00b393b1af
Mike Lorbetske <mlorbe@microsoft.com>
2016-12-28 14:40:49 -0800

    Disable test parallelization for dotnet-new tests

--- test/dotnet-new.Tests/New3/New3TestBase.cs
+++ test/dotnet-new.Tests/New3/New3TestBase.cs
@@ -2,6 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.DotNet.Tools.Test.Utilities;
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
 
 namespace Microsoft.DotNet.New3.Tests
 {
