7676e085c3316a00aa18e7b44ad1aab08251d8aa
Livar Cunha <livar@live.com>
2017-10-16 21:13:51 -0700

    Updating a tools test to expect the right version of the runtime: 1.1.5.

--- test/Microsoft.DotNet.Cli.Utils.Tests/GivenAProjectToolsCommandResolver.cs
+++ test/Microsoft.DotNet.Cli.Utils.Tests/GivenAProjectToolsCommandResolver.cs
@@ -308,7 +308,7 @@ namespace Microsoft.DotNet.Tests
 
             result.Should().NotBeNull();
 
-            result.Args.Should().Contain("--fx-version 1.1.5-servicing-001651-00");
+            result.Args.Should().Contain("--fx-version 1.1.5");
         }
 
         [Fact]
