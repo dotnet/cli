a1733e3d892602271ac9e18e6e9e418734ad63fe
Andy Gerlicher <angerlic@microsoft.com>
2018-01-10 22:34:42 -0800

    Use a multi-proc aware MSBuild logger Make use of the MSBuild distributed logger functionality and add a forwarding logger. When in a multi-proc build, the forwarding logger will decide which events to forward to the main node to be logged. Without this, all events are routed and a perf penalty is incurred.

---
+++ src/dotnet/commands/dotnet-msbuild/MSBuildForwardingLogger.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Framework;
+
+namespace Microsoft.DotNet.Tools.MSBuild
+{
+    public sealed class MSBuildForwardingLogger : IForwardingLogger
+    {
+        public LoggerVerbosity Verbosity { get; set; } = LoggerVerbosity.Minimal;
+
+        public string Parameters { get; set; } = null;
+
+        public IEventRedirector BuildEventRedirector { get; set; }
+
+        public int NodeId { get; set; }
+
+        public void Initialize(IEventSource eventSource)
+        {
+            // Only forward telemetry events
+            if (eventSource is IEventSource2 eventSource2)
+                eventSource2.TelemetryLogged += (sender, args) => BuildEventRedirector.ForwardEvent(args);
+        }
+
+        public void Initialize(IEventSource eventSource, int nodeCount)
+        {
+            Initialize(eventSource);
+        }
+
+        public void Shutdown()
+        {
+        }
+    }
+}
