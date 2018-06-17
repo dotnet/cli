1e698d8f8cad105de7936000be532aa98b8f5bf6
William Li <wul@microsoft.com>
2018-06-17 11:51:20 -0700

    Unskip to see if it is supported now

--- test/EndToEnd/GivenSelfContainedAppsRollForward.cs
+++ test/EndToEnd/GivenSelfContainedAppsRollForward.cs
@@ -16,7 +16,7 @@ namespace EndToEnd
     public class GivenSelfContainedAppsRollForward : TestBase
     {
 
-        [Theory(Skip = "Runtime 1.1 support for openSUSE and Fedora 27 needed")]
+        [Theory]
         //  MemberData is used instead of InlineData here so we can access it in another test to
         //  verify that we are covering the latest release of .NET Core
         [MemberData(nameof(SupportedNetCoreAppVersions))]
