2df4137c460f7680ed41c818e66d24f433f9334b
Rainer Sigwald <raines@microsoft.com>
2018-09-26 12:32:33 -0500

    Avoid VT-100 output from run-build.sh
    This is an attempt to work around/isolate the cause of intermittent
    hangs in the CLI official build. Since the hang appears to be happening
    when emitting a control code to STDOUT, let's try to set STDOUT up in
    such a way that things mostly don't try to emit control codes to it.
    
    Related:
    - https://github.com/Microsoft/msbuild/issues/3563
    - https://github.com/dotnet/corefx/issues/31559

--- run-build.sh
+++ run-build.sh
@@ -192,8 +192,11 @@ fi
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 
 if [ $BUILD -eq 1 ]; then
-    dotnet msbuild build.proj /bl:msbuild.generatepropsfile.binlog /p:Architecture=$ARCHITECTURE $CUSTOM_BUILD_ARGS /p:GeneratePropsFile=true /t:WriteDynamicPropsToStaticPropsFiles ${argsnotargets[@]}
-    dotnet msbuild build.proj /bl:msbuild.mainbuild.binlog /m /v:normal /fl /flp:v=diag /p:Architecture=$ARCHITECTURE $CUSTOM_BUILD_ARGS $args
+    # Piped through `cat` to avoid terminal detection emitting control characters
+    # to try to work around https://github.com/dotnet/corefx/issues/31559 and therefore
+    # https://github.com/Microsoft/msbuild/issues/3563
+    dotnet msbuild build.proj /bl:msbuild.generatepropsfile.binlog /p:Architecture=$ARCHITECTURE $CUSTOM_BUILD_ARGS /p:GeneratePropsFile=true /t:WriteDynamicPropsToStaticPropsFiles ${argsnotargets[@]} | cat
+    dotnet msbuild build.proj /bl:msbuild.mainbuild.binlog /m /v:normal /fl /flp:v=diag /p:Architecture=$ARCHITECTURE $CUSTOM_BUILD_ARGS $args | cat
 else
     echo "Not building due to --nobuild"
     echo "Command that would be run is: 'dotnet msbuild build.proj /m /p:Architecture=$ARCHITECTURE $CUSTOM_BUILD_ARGS $args'"
