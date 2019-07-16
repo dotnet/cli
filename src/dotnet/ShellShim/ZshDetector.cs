c603a3d65ab0c774d63ab0cdf5c5f40d869881e3
William Li <wul@microsoft.com>
2019-07-16 16:09:38 -0700

    Give specific instruction for zsh on macOS for global tools PATH

---
+++ src/dotnet/ShellShim/ZshDetector.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using Microsoft.DotNet.Cli.Utils;
+
+namespace Microsoft.DotNet.ShellShim
+{
+    internal class ZshDetector
+    {
+        private readonly IEnvironmentProvider _environmentProvider;
+        private const string ZshFileName = "zsh";
+
+        public ZshDetector(IEnvironmentProvider environmentProvider = null)
+        {
+            _environmentProvider = environmentProvider;
+        }
+
+        /// <summary>
+        ///     Return if the user use zsh as "The user's shell" instead of the current shell.
+        ///     By detecting $SHELL's value
+        /// </summary>
+        public bool IsZshTheUsersShell()
+        {
+            string environmentVariable = _environmentProvider.GetEnvironmentVariable("SHELL");
+            if (string.IsNullOrWhiteSpace(environmentVariable))
+            {
+                return false;
+            }
+
+            if (Path.GetFileName(environmentVariable).Equals(ZshFileName, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ test/Microsoft.DotNet.ShellShim.Tests/ZshDetectorTests.cs
@@ -0,0 +1,44 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
+using Microsoft.DotNet.Cli.Utils;
+using Moq;
+using Xunit;
+
+namespace Microsoft.DotNet.ShellShim.Tests
+{
+    public class ZshDetectorTests
+    {
+        [Theory]
+        [InlineData("/bin/zsh")]
+        [InlineData("/other-place/zsh")]
+        public void GivenFollowingEnvironmentVariableValueItCanDetectZsh(string environmentVariableValue)
+        {
+            Mock<IEnvironmentProvider> provider = new Mock<IEnvironmentProvider>(MockBehavior.Strict);
+
+            provider
+                .Setup(p => p.GetEnvironmentVariable("SHELL"))
+                .Returns(environmentVariableValue);
+
+            ZshDetector zshDetector = new ZshDetector(provider.Object);
+            zshDetector.IsZshTheUsersShell().Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("/bin/bash")]
+        [InlineData("/other/value")]
+        [InlineData(null)]
+        public void GivenFollowingEnvironmentVariableValueItCanDetectItIsNotZsh(string environmentVariableValue)
+        {
+            Mock<IEnvironmentProvider> provider = new Mock<IEnvironmentProvider>(MockBehavior.Strict);
+
+            provider
+                .Setup(p => p.GetEnvironmentVariable("SHELL"))
+                .Returns(environmentVariableValue);
+
+            ZshDetector zshDetector = new ZshDetector(provider.Object);
+            zshDetector.IsZshTheUsersShell().Should().BeFalse();
+        }
+    }
+}
