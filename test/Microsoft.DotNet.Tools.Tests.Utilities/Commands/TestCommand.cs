765f108e8e749476dd1a1b913fe6118fb234dee1
William Godbe <wigodbe@microsoft.com>
2019-11-11 11:56:56 -0800

    Use right property

--- test/Microsoft.DotNet.Tools.Tests.Utilities/Commands/TestCommand.cs
+++ test/Microsoft.DotNet.Tools.Tests.Utilities/Commands/TestCommand.cs
@@ -148,7 +148,7 @@ namespace Microsoft.DotNet.Tools.Test.Utilities
             RemoveCliGeneratedEnvironmentVariablesFrom(psi);
             
             // Set DOTNET_ROOT to ensure sub process find the same host fxr
-            string dotnetDirectoryPath = Path.GetDirectoryName(RepoDirectoriesProvider._dotnetRoot);
+            string dotnetDirectoryPath = Path.GetDirectoryName(RepoDirectoriesProvider.DotnetRoot);
             if (System.Environment.Is64BitProcess)
             {
                 psi.Environment.Add("DOTNET_ROOT", dotnetDirectoryPath);
