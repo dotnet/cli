8d2858a07673fffd827ef3f60655a2d097f8ea62
Larson Carter <larsoncarter@blazermail.net>
2020-06-04 10:00:43 -0500

    Remove function

--- scripts/register-completions.zsh
+++ scripts/register-completions.zsh
@@ -1,12 +1,3 @@
-# zsh parameter completion for the dotnet CLI
-
-_dotnet_zsh_complete() 
-{
-
-    #compdef dotnet
-    local completions=("$(dotnet complete "$words")")
-    compadd "${(ps:\n:)completions}"
-    
-}
-
-compctl -K _dotnet_zsh_complete dotnet
+#compdef dotnet
+local completions=("$(dotnet complete "$words")")
+compadd "${(ps:\n:)completions}"
